4cfd5faaf16e6e786364bfe388a8d1c6642ee6e7
Nihad <nihadishi@gmail.com>
2024-01-14 00:28:45 +0400

    splash

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,16 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import SplashScreen from 'react-native-splash-screen'
 
 const Main = () => {
+    useEffect(() => {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 2000);
+    
+      
+    }, [])
+    
   return (
     <View>
       <Text>Main</Text>
